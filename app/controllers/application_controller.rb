de7e9fd6e313a4fb189dd3dc0eaa6d622cc91f20
Michael Dilley <mike.dilley@gmail.com>
2018-12-30 15:03:28 -0600

    Add login helper methods and login routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,24 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+
+    def is_loggedin?
+      !!session[:id]
+    end
+
+    def login(id)
+      session[:id] = id
+    end
+
+    def logout
+      session.clear
+    end
+
+    def current_user
+      User.find_by(session[:id])
+    end
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,40 @@
 class UsersController < ApplicationController
 
   get '/users/signup' do
-    erb :"/users/signup"
+    if is_loggedin?
+      erb :"/users/signup"
+    else
+      redirect to "/decks/show"
+    end
   end
 
   post '/users/signup' do
-    
+    if !is_loggedin?
+      @user = User.create(params)
+      login(@user.id)
+      erb :"/decks/show"
+    else
+      redirect to "/"
+    end
+  end
+
+  get '/users/login' do
+    if !is_loggedin?
+      erb :"/users/login"
+    else
+      erb :"/decks/show"
+    end
+  end
+
+  post '/users/login' do
+    binding.pry
+    @user = User.find_by(:username => params["username"])
+    if !is_loggedin?
+      login(params)
+      erb :"/users/login"
+    else
+      erb :"/decks/show"
+    end
   end
 
 end
