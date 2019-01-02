a6720b4b49208c8d1db4b1f77521d52953b7f6f8
Michael Dilley <mike.dilley@gmail.com>
2019-01-02 12:03:02 -0600

    Add edit and delete decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -14,11 +14,43 @@ class DecksController < ApplicationController
   end
 
   post '/decks' do
-    @deck = Deck.create(params)
-    @user = current_user
-    @user.decks << @deck
     if is_loggedin?
+      @deck = Deck.create(params)
+      @user = current_user
+      @user.decks << @deck
       erb :"/decks/show"
+    else
+      redirect to "/users/login"
+    end
+  end
+
+  get '/decks/:id/edit' do
+    @deck = Deck.find(params[:id])
+    erb :"/decks/edit"
+  end
+
+  patch '/decks/:id' do
+    deck = Deck.find(params[:id])
+    if is_loggedin? && (deck.user_id == current_user.id)
+      if params[:name].strip != ""
+        deck.update("name" => params[:name])
+      end
+      if params[:color].strip != ""
+        deck.update("color" => params[:color])
+      end
+      if params[:size].strip != ""
+        deck.update("size" => params[:size])
+      end
+      redirect to "/decks"
+    else
+      redirect to "/users/login"
+    end
+  end
+
+  delete '/decks/:id' do
+    deck = Deck.find(params[:id])
+    if is_loggedin? && (deck.user_id == current_user.id)
+      deck.delete
     else
       redirect to "/users/login"
     end
