ae39bde49500623dbe8f0992811287059d9be422
Michael Dilley <mike.dilley@gmail.com>
2019-01-03 11:16:21 -0600

    Refactor add card to deck, create helper method get_card_attr

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -59,22 +59,7 @@ class DecksController < ApplicationController
         deck.update("size" => params["deck"][:size])
       end
       if params["card"][:name] != ""
-        # Refactor this 
-        url = "https://api.scryfall.com/cards/named?fuzzy=#{card_name_to_search_name(params["card"]["name"])}"
-        uri = URI(url)
-        response = Net::HTTP.get(uri)
-        c = JSON.parse(response)
-        card = Card.create("name" => c["name"])
-        card.mana_cost = c["mana_cost"]
-        card.card_type = c["type_line"]
-        card.card_text = c["oracle_text"]
-        card.colors = c["colors"]
-        card.expansion = c["set_name"]
-        card.rarity = c["rarity"]
-        c["flavor_text"] ? card.flavor_text = c["flavor_text"] : card.flavor_text = "n/a"
-        card.img_url = c["image_uris"]["normal"]
-        c["power"] ? card.power = c["power"] : card.power = "n/a"
-        c["toughness"] ? card.toughness = c["toughness"] : card.toughness = "n/a"
+        card = get_card_attr(params["card"]["name"])
         deck.cards << card
       end
       redirect to "/decks"
